d15750fcbec03063080b0b86f6aa001904371a06
“Cory <coryurton@gmail.com>
2022-08-28 23:13:12 -0500

    a bunch of not working code for exception handling

--- src/main/java/com/exampleSpringWeb/SpringWebdemo/web/DogsController.java
+++ src/main/java/com/exampleSpringWeb/SpringWebdemo/web/DogsController.java
@@ -6,7 +6,12 @@ import com.exampleSpringWeb.SpringWebdemo.service.DogsService;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,20 +29,19 @@ public class DogsController {
         dogsService.add(dto);
     }
 
-    @GetMapping
-    public List<Dog> getDogs() {
-        return dogsService.getDogs();
-    }
-
 //    @GetMapping
-//    public ResponseEntity<List<Dog>> getDogs() {
-//        return new ResponseEntity<>(dogsService.getDogs(), HttpStatus.OK);
-        //        List<Dog> dogs;
+//    public List<Dog> getDogs() {
+//        return dogsService.getDogs();
+//    }
+
+    @GetMapping
+    public ResponseEntity<List<Dog>> getDogs() {
+        return new ResponseEntity<>(dogsService.getDogs(), HttpStatus.OK);
+                List<Dog> dogs;
 
 //        try{
 //            dogs = dogsService.getDogs();
-//
-//        } catch (Exception e) {
+//        } catch (DogsServiceException ex) {
 //            return new ResponseEntity<>(null, null, HttpStatus.INTERNAL_SERVER_ERROR);
 ////               return ResponseEntity.badRequest().build();
 //        } catch (DogsNotFoundException e) {
@@ -45,9 +49,9 @@ public class DogsController {
 //        }
 //          return new ResponseEntity<>(dogs, HttpStatus.OK);
 //        }
-        //duplication = bad. better to make a ControllerAdvice
+        //duplication = bad. generic catch blocks, and will need them at other endpoints (post, delete). better to make a ControllerAdvice
 
-//    }
+    }
 
 
     @GetMapping("/{id}")
@@ -59,6 +63,41 @@ public class DogsController {
     public void delete(@PathVariable(required = true) long id) {
         dogsService.delete(id);
     }
+
+
+
+
+    //sample POST with curl:
+/*    curl -X POST \
+    http://localhost:8080/dogs \
+           -H 'Content-Type: application/json' \
+            -H 'Postman-Token: c6813aea-146c-49cd-9eba-1370aad4bff9' \
+          -H 'cache-control: no-cache' \
+           -d '{
+           "id": 6,
+           "name": "Hooch",
+            "age": 11
+}' */
+
+    //delete request with curl:
+    /*
+    curl -X DELETE \
+    http://localhost:8080/dogs/5 \
+            -H 'Content-Type: application/json' \
+            -H 'Postman-Token: b4b93304-7ee7-45c2-917b-c3bc2985a250' \
+            -H 'cache-control: no-cache' */
+
+//general form for curl command making a POST request:
+//    curl -X POST [options] [URL]
+
+    //curl post with -f flag: field=value pairs. example sending a POST request to a form with name and email fields:
+//    curl -X POST -F 'name=linuxize' -F 'email=linuxize@example.com' https://example.com/contact.php
+
+
+    //curl POST request with -d flag sends data using the application/x-www-form-urlencoded content type:
+    //curl -X POST -d 'name=linuxize' -d 'email=linuxize@example.com' https://example.com/contact.php
+
+
 
 
 }
